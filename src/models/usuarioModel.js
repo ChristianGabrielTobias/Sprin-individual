d6e0071d20da93cff42fe30d89923ee4cb11e50e
lozanodev <madakey@live.com>
2021-11-26 15:16:14 -0300

    subindo accertos no css com metricas

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -27,11 +27,21 @@ function cadastrar(usuario, email, senha) {
     return database.executar(instrucao);
 }
 
+// function metricas(usuario) {
+    // console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", usuario);
+    // var instrucao = `
+        // SELECT count.(usuario) from usuario ;
+    // `;
+    //  console.log("Executando a instrução SQL: \n"+instrucao);
+    // return database.executar(instrucao); 
+ 
+
 
 
 module.exports = {
     entrar,
     cadastrar,
     listar,
+    // metricas;
 
 };
